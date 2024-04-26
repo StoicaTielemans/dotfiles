55e5e526beb60aa48c1a70e2d3ee8b2c49976dd1
StoicaTielemans <r0983518@student.thomasmore.be>
2024-04-27 00:45:54 +0200

    fixes

---
+++ nvim/lua/plugins/colorize.lua
@@ -0,0 +1,10 @@
+return {
+  'norcalli/nvim-colorizer.lua',
+  config = function()
+    require('colorizer').setup {
+      '*', -- Highlight all files, but customize some others.
+      css = { css = true, css_fn = true }, -- Enable parsing rgb(...) functions in css.
+      html = { css = true, css_fn = true }, -- Disable parsing "names" like Blue or Gray
+    }
+  end,
+}
